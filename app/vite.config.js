242dd5124da465e1b277eeca60700a6d06e1b075
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2025-05-06 13:15:43 -0600

    new domain

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RespiLens/',
+  base: '/',
   build: {
     outDir: 'dist'  // Ensure consistent output directory
   },
@@ -14,7 +14,7 @@ export default defineConfig({
     },
     proxy: {
       '/processed_data': {
-        target: 'http://localhost:5173/RespiLens',
+        target: 'http://localhost:5173/',
         rewrite: (path) => path
       }
     }
